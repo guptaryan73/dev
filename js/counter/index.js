e7f95832edf3bdaaef9f9ab1446ab2375c868cf2
guptaryan73 <guptaryan73@gmail.com>
2024-07-02 21:32:32 +0530

    added files

---
+++ js/counter/index.js
@@ -0,0 +1,8 @@
+function increase() {
+    counter.textContent = parseInt(counter.textContent) + 1
+}
+function save() {
+    let newStringtoBeAdded = counter.innerText + " - ";
+    console.log(entries.textContent += newStringtoBeAdded)
+}
+
